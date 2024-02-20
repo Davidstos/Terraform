c648bfc710d0d0457edd8f634990b247a0a75fcf
David <62214963+Davidstos@users.noreply.github.com>
2024-02-20 20:52:36 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name = "rg-shared-mgmt-francecentral-001"
+    storage_account_name = "davidstos"
+    container_name = "tfstate"
+    key = "terraform.tfstate"
+  }
+  required_providers {
+    azurerm = {
+      version = "~>3.2"
+      source = "hashicorp/azurerm"
+       }
+    }
+}
+provider "azurerm"{
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+    location = "France Central"
+    name  = "rg-test—github-actions"
+}
